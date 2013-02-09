c6ecaed598484240ccfd201c91ca027cafd2420f
Samuel Marisa <azcorbin@gmail.com>
2013-02-09 14:46:10 +0200

    added comments to tool.py

--- tool.py
+++ tool.py
@@ -2,6 +2,17 @@
 #-*- coding: UTF-8 -*-
 
 
+"""
+	tool.py  :  easy management of your project: write less, do more!
+
+		author:  smarisa
+		  date:  2013-02-09
+
+
+	USAGE:  tool.py (FUNCTION ARG*)*
+"""
+
+
 # Python
 import os, sys
 
@@ -15,6 +26,11 @@ from mutils import sysrunrv, gpush, gpull, simple_arg_parser
 #
 
 def set_perms( args=[] ):
+	"""
+		Sets the file permissions as they should be: maximizing security while
+		allowing the required permissions in order for the site to function.
+	"""
+
 	sysrunrv( 'find . -type d -exec chmod 755 "{}" \;' )
 	sysrunrv( 'find . -type f -exec chmod 600 "{}" \;' )
 	sysrunrv( 'find ./pgs/              -maxdepth 1 -type f -name "*php" -exec chmod o+r "{}" \;' )
@@ -24,10 +40,14 @@ def set_perms( args=[] ):
 	sysrunrv( 'chmod o+r ./index.php' )
 	sysrunrv( 'chmod o+r ./favicon.ico' )
 	sysrunrv( 'chmod u+x ./tool.py' )
+
 	return
 
 
 def custom( args=[] ):
+	"""
+		Custom temporary functionalities can be handily added here.
+	"""
 
 	return
 

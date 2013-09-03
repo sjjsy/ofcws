172deb9dfb94673bcfe2c6385cabac5f894c1ae3
Samuel Marisa <azcorbin@gmail.com>
2013-09-03 21:45:48 +0300

    Jihaa!

--- pgs/events.php
+++ pgs/events.php
@@ -6,14 +6,11 @@
 
 <p>
 	Below is a listing of the upcoming events from our event schedule. However, it
-	doesn't repeat recurring events which for this spring are: "wrestling" on
-	Mondays & "kickboxing" for Wednesdays.
+	doesn't repeat recurring events which for this fall are: "wrestling" on
+	Mondays & "kickboxing" for Wednesdays and Saturdays.
 </p>
 
 <p>
-	More information at the
-	<a href="http://www.facebook.com/groups/107657179302080/">Facebook group</a>
-	and at <a href="http://otaniemifightclub.nimenhuuto.com/">Nimenhuuto</a>.
 	You may subscribe to the calendar's XML feed directly using <a href="https://www.google.com/calendar/feeds/m8540j7mbg1akg32rvhfb6uir4%40group.calendar.google.com/public/basic">this link</a>.
 </p>
 
@@ -22,7 +19,14 @@
 	<a href="/feed/rss.xml" title="The OFC Fight Feed" type="application/rss+xml">
 		an RSS feed
 	</a>
-	to which you can subscribe and in order to follow the latest news.
+	to which you can subscribe in order to follow the latest news.
+</p>
+
+<p>
+   All that said, this website is designed to be a rather static source of information. The
+	<a href="http://www.facebook.com/groups/107657179302080/">Facebook group</a>
+	and <a href="http://otaniemifightclub.nimenhuuto.com/">Nimenhuuto</a>
+   services offer the most up-to-date and valid information.
 </p>
 
 <h3>Upcoming Events</h3>

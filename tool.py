f68a42fb2ce9c77511282a2a564c28e641167c08
Samuel Marisa <azcorbin@gmail.com>
2013-09-04 10:42:33 +0300

    Fixed a permissions issue.

--- tool.py
+++ tool.py
@@ -38,6 +38,7 @@ def set_perms( args=[] ):
     'find ./static/gfx/         -maxdepth 1 -type f -name "*jpg" -exec chmod o+r "{}" \;',
     'find ./static/gfx/gallery/ -maxdepth 1 -type f -name "*jpg" -exec chmod o+r "{}" \;',
     'find ./static/css/         -maxdepth 1 -type f -name "*css" -exec chmod o+r "{}" \;',
+    'find ./static/js/          -maxdepth 1 -type f -name "*js"  -exec chmod o+r "{}" \;',
 #    'find ./static/php/                     -type f              -exec chmod o+r "{}" \;',
     'find ./feed/               -maxdepth 1 -type f              -exec chmod o+r "{}" \;',
     'chmod o+r ./index.php',

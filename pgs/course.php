7c7ea3ba2b2504cb24310178e7f04a6a8d9f665a
Samuel Marisa <azcorbin@gmail.com>
2013-10-17 14:31:24 +0300

    Nothing special.

--- pgs/course.php
+++ pgs/course.php
@@ -18,7 +18,7 @@
   In case you're completely new to MMA or martial arts in general
   and wonder why you should invest your time in it,
   start by laying an eye on our
-	<a href="/pgs/why.php">why -page</a>.
+	<a href="/pgs/why.php">why page</a>.
 </p>
 
 

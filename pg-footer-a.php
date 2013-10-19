c40b91ca199536da37e2e11aa2fd6c44c6b5e937
Samuel Marisa <azcorbin@gmail.com>
2013-10-19 12:46:32 +0300

    Nothing special.

--- pg-footer-a.php
+++ pg-footer-a.php
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-bottom">
       <div style="text-align: center;">
         <small>
-          Created 2013-02-07, Modified 2013-10-17 <br/>
+          Created 2013-02-07, Modified 2013-10-19 <br/>
           All Rights Reserved / Samuel Marisa / OFC
         </small>
       </div>

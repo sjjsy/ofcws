83e20693a5f87e2d0d2682cf153de4f1a1825163
Samuel Marisa <azcorbin@gmail.com>
2014-09-08 19:24:00 +0300

    No comment.

--- pgs/honoraries.php
+++ pgs/honoraries.php
@@ -9,24 +9,19 @@
     The following individuals have been granted honorary membership:
   </p>
 
-  <ul>
-    <li>
-      <b>Korhonen, Atte</b>
-      <p>
-        For his strong social contributions to the
-        club's health through his remarkably frequent and exemplary
-        participation in the club's weekly activities. Granted 2014-05-17.
-      </p>
-    </li>
-    <li>
-      <b>Martinez, Bosco</b>
-      <p>
-        For his efforts as the club's founder and his
-        perseverance in incrementally helping it establish itself among
-        the other associations under AYY. Granted 2014-05-17.
-      </p>
-    </li>
-  </ul>
+  <b>Korhonen, Atte</b>
+  <p>
+    For his strong social contributions to the
+    club's health through his remarkably frequent and exemplary
+    participation in the club's weekly activities. Granted 2014-05-17.
+  </p>
+
+  <b>Martinez, Bosco</b>
+  <p>
+    For his efforts as the club's founder and his
+    perseverance in incrementally helping it establish itself among
+    the other associations under AYY. Granted 2014-05-17.
+  </p>
 
 </div>
 

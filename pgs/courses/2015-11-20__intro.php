af8633757f5ffe408526c573c796ee22405d32cf
Samuel Marisa <azcorbin@gmail.com>
2015-11-08 22:14:13 +0200

    No comment.

---
+++ pgs/courses/2015-11-20__intro.php
@@ -0,0 +1,239 @@
+<?php
+  $thetitle = 'MMA For You? | ';
+  include( "../../pg-header.php" );
+?>
+
+
+<h2>OFC For You?</h2>
+
+
+<br/>
+<br/>
+
+
+<div class="container text-success" style="font-size: 1.3em;">
+  <div class="row">
+    <div class="col-md-3">
+      What?
+    </div>
+    <div class="col-md-9">
+      A two hour intro to MMA with OFC
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      For whom?
+    </div>
+    <div class="col-md-9">
+      You and your friends
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      Any costs?
+    </div>
+    <div class="col-md-9">
+      Zero, zip, nada
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      When?
+    </div>
+    <div class="col-md-9">
+      15:00-17:00 on the 20th of November
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      Where?
+    </div>
+    <div class="col-md-9">
+      At Otahalli, Unisport Otaniemi
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      Hmmh...
+    </div>
+    <div class="col-md-9">
+      <b>
+        Read more from below!
+      </b>
+    </div>
+  </div>
+</div>
+
+
+
+
+<h3 id="what">
+  What?
+</h3>
+
+<p>
+  The club is organizing a chance for everybody to get more familiar
+  with the club and the sport we call MMA and martial arts in general.
+</p>
+
+<p>
+  <i>
+    The only requirements for participation are
+    good health and a social, sporty and positive attitude.
+  </i>
+</p>
+
+
+
+
+<h3 id="facts">
+  Facts
+</h3>
+
+
+<h4>
+  Schedule
+</h4>
+
+<table class="table">
+  <thead>
+    <th>
+      Time
+    </th>
+    <th>
+      Topic
+    </th>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+        15:00-15:15
+      </td>
+      <td>
+        What the MMA!?
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15:15-15:45
+      </td>
+      <td>
+        Warmup and pair exercises
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15:45-16:15
+      </td>
+      <td>
+        Arts up high
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16:15-16:45
+      </td>
+      <td>
+        Arts down low
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16:45-17:00
+      </td>
+      <td>
+        Muscle joy
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<br/>
+
+
+<h4>
+  Location
+</h4>
+
+<p>
+  The session will be held in the sports hall named "Monitoimihalli"
+  in Unisport Otaniemi's Otahalli facilities. The address is
+  Otaranta 6, 02150 Espoo. The tatami area isn't visible when you get
+  into the hall. You must walk to the far-left corner to spot it.
+  In case you have a valid Unisport subscription, you can register for
+  the session via the Unisport reader, otherwise just say you're going
+  to attend "Vapaaottelu" to the receptionists. Finding the place and
+  changing your clothes at the dressing rooms might take time, so
+  please adjust your schedules accordingly.
+</p>
+
+
+<h4>
+  Requirements
+</h4>
+
+<p>
+  There are no odd requirements for participation except normal health and a
+  social positive attitude.
+  There's no need for special equipment.
+  A sturdy pair of shorts and a t-shirt will do.
+  No socks on the tatami.
+  Just remember to round your nails and lay an eye on this document
+	<a href="http://bit.ly/1co4oyc">in English</a> or
+	<a href="http://bit.ly/16YllRa">in Finnish</a>.
+</p>
+
+
+<h4>
+  Content
+</h4>
+
+<p>
+  The schedule above is a rough sketch to let you know what to expect.
+  The objective of the session is to give you a quick & compact introduction to
+  the sport we refer to as MMA and martial arts in general. You will also learn
+  more about our club, who we are and how and why we train.
+</p>
+
+<p>
+  Even though the session is novice friendly, we'll make you sweat. The session
+  also contains some sparring-like pair exercises. This is going to include
+  a few soft-contact exercises. But fear not: at OFC we train as ladies and
+  gentlemen.
+</p>
+
+
+<h4>
+  Conclusions
+</h4>
+
+<p>
+  A free, compact, hard-to-beat chance to get to know more about the club and
+  MMA.
+</p>
+
+<p>
+  You still think you have
+  <strong class="text-error">
+    excuses
+  </strong>
+  for not coming?
+  <br/>
+  You:
+  <strong>
+    No. None whatsoever! .. In fact, I'm gonna have my mates join as well!
+  </strong>
+  <br/>
+  Thadda kid! ;)
+  <strong>
+  Go get em!
+  </strong>
+</p>
+
+
+<?php	include("../../pg-footer-a.php"); ?>
+
+
+
+
+<!-- EOF -->
